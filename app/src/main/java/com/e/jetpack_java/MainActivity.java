5d272e03dcf7d9ab908490bce9bf281d5bf0b877
Seungki An <55059074+seungking@users.noreply.github.com>
2020-08-31 19:57:24 +0900

    20200831
    - Room 비동기처리
    - AsyncTask

--- app/src/main/java/com/e/jetpack_java/MainActivity.java
+++ app/src/main/java/com/e/jetpack_java/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
                 .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
                                           // 이거 붙혀주면 메인 쓰레드에서 동작이 가능하다 / 실무에선 사용 x 연습할때 주로사용
+                                            //main에서 돌리면 성능이 느려질 수 있다.
                 .build();
 
         //UI 갱신
@@ -45,8 +47,11 @@ public class MainActivity extends AppCompatActivity {
 
         //추가
         add.setOnClickListener(v->{
-            db.todoDao().insert(new Todo(input.getText().toString()));
+//            db.todoDao().insert(new Todo(input.getText().toString()));
 //            result.setText(db.todoDao().getAll().toString());
+
+            //비동기 실행
+            new InsertAsyncTask(db.todoDao()).execute(new Todo(input.getText().toString()));
         });
 
         //삭제 - title 같은 todo 찾아서 삭제
@@ -56,5 +61,23 @@ public class MainActivity extends AppCompatActivity {
             //result.setText(db.todoDao().getAll().toString());
         });
 
+    }
+
+    //AsynTask<넘겨받을 객체, 중간에 처리할 데이터, 프로그래스에서 처리할 값, 결과값>
+    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void>{
+
+        //Dao처리를 위해서 생성자에서 dao 받와아서 지정
+        private TodoDao mTodoDao;
+
+        public InsertAsyncTask(TodoDao todoDao) {
+            this.mTodoDao = todoDao;
+        }
+
+        //여기에서 비동기 처리
+        @Override
+        protected Void doInBackground(Todo... todos) {
+            mTodoDao.insert(todos[0]);
+            return null;
+        }
     }
 }
