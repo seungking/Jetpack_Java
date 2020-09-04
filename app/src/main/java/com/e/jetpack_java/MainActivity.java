3e24e0b73a9ec563649cc7e50c0529f0dafee160
Seungki An <55059074+seungking@users.noreply.github.com>
2020-09-04 20:00:07 +0900

    20200904
    - ViewModel 분리

--- app/src/main/java/com/e/jetpack_java/MainActivity.java
+++ app/src/main/java/com/e/jetpack_java/MainActivity.java
@@ -2,6 +2,7 @@ package com.e.jetpack_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 
 import android.os.AsyncTask;
@@ -29,55 +30,26 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        //db 생성, 설정 이름의 파일이 실제로 생성된다
-        AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
-                .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
-                                          // 이거 붙혀주면 메인 쓰레드에서 동작이 가능하다 / 실무에선 사용 x 연습할때 주로사용
-                                            //main에서 돌리면 성능이 느려질 수 있다.
-                .build();
+        MainViewModel viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(MainViewModel.class);
 
         //UI 갱신
         //livedata obserce
         //setting에서 모듈 자바 1.8 실행하면 아래와 같이 표현하는 람다 사용가능
-        db.todoDao().getAll().observe(this, todos -> {
+        viewModel.getAll().observe(this, todos -> {
             result.setText(todos.toString());
         });
 
 
-
         //추가
         add.setOnClickListener(v->{
-//            db.todoDao().insert(new Todo(input.getText().toString()));
-//            result.setText(db.todoDao().getAll().toString());
-
-            //비동기 실행
-            new InsertAsyncTask(db.todoDao()).execute(new Todo(input.getText().toString()));
+            viewModel.insert(new Todo(input.getText().toString()));
         });
 
         //삭제 - title 같은 todo 찾아서 삭제
         delete.setOnClickListener(v->{
-            db.todoDao().delete(db.todoDao().get(input.getText().toString()));
-            //livedata 추가해서 주석처리
-            //result.setText(db.todoDao().getAll().toString());
+            viewModel.delete(input.getText().toString());
         });
 
     }
 
-    //AsynTask<넘겨받을 객체, 중간에 처리할 데이터, 프로그래스에서 처리할 값, 결과값>
-    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void>{
-
-        //Dao처리를 위해서 생성자에서 dao 받와아서 지정
-        private TodoDao mTodoDao;
-
-        public InsertAsyncTask(TodoDao todoDao) {
-            this.mTodoDao = todoDao;
-        }
-
-        //여기에서 비동기 처리
-        @Override
-        protected Void doInBackground(Todo... todos) {
-            mTodoDao.insert(todos[0]);
-            return null;
-        }
-    }
 }
---
+++ app/src/main/java/com/e/jetpack_java/MainViewModel.java
@@ -0,0 +1,66 @@
+package com.e.jetpack_java;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.room.Room;
+
+import java.util.List;
+
+//main activity 뷰모델
+//context 가 필요할때 androidviewmodel로 상속
+public class MainViewModel extends AndroidViewModel {
+
+    AppDatabase db;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        //db 생성, 설정 이름의 파일이 실제로 생성된다
+        db = Room.databaseBuilder(application, AppDatabase.class, "todo-db")
+                .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
+                // 이거 붙혀주면 메인 쓰레드에서 동작이 가능하다 / 실무에선 사용 x 연습할때 주로사용
+                //main에서 돌리면 성능이 느려질 수 있다.
+                .build();
+
+    }
+
+    //db를 직접 다루지 못하도록 한번 더 감싸주기
+    public LiveData<List<Todo>> getAll(){
+        return db.todoDao().getAll();
+    }
+
+    public void insert(Todo todo){
+        //비동기 실행
+        //new안에 들어가는게 인자로 넘겨주고
+        //execute안에 들어가는게 doinbackground로 들어가는 인자
+        new InsertAsyncTask(db.todoDao()).execute(todo);
+    }
+    public void delete(String todo){
+        //비동기 실행
+        //new안에 들어가는게 인자로 넘겨주고
+        //execute안에 들어가는게 doinbackground로 들어가는 인자
+        db.todoDao().delete(db.todoDao().get(todo));
+    }
+
+    //AsynTask<넘겨받을 객체, 중간에 처리할 데이터, 프로그래스에서 처리할 값, 결과값>
+    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void> {
+
+        //Dao처리를 위해서 생성자에서 dao 받와아서 지정
+        private TodoDao mTodoDao;
+
+        public InsertAsyncTask(TodoDao todoDao) {
+            this.mTodoDao = todoDao;
+        }
+
+        //여기에서 비동기 처리
+        @Override
+        protected Void doInBackground(Todo... todos) {
+            mTodoDao.insert(todos[0]);
+            return null;
+        }
+    }
+}
